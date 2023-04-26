3ec886a6cd93655bba72ec386cae54a1a74c81bd
ron pastore <ron@rons-MBP.fios-router.home>
2023-04-25 22:00:59 -0400

    React app with question component

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import QuestionAnswer from './components/question/questionAnswer';
+
+function App() {
+  return (
+    <div className="App">    
+      <iframe sandbox="allow-scripts allow-same-origin allow-popups" width="336" height="550" src="https://read.amazon.com/kp/card?asin=B07H6Z1GR8&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_F5RGHQ7YX2JATHB1PYQX" ></iframe>
+      <h1>Ask a question about the book...</h1>
+      <p>Inspired by the AI experiment, <a href="https://www.Askmybook.com">AskMyBook.com</a></p>
+      <QuestionAnswer question="Who is this book for?" />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/question/questionAnswer.tsx
@@ -0,0 +1,60 @@
+import React, { useState, ChangeEvent } from 'react';
+
+interface Props {
+  question: string;
+}
+
+interface Response {
+  answer: string;
+}
+
+const API_URL = 'http://localhost:3000/questions';
+
+const QuestionAnswer: React.FC<Props> = ({ question }) => {
+  
+  const [answer, setAnswer] = useState<string>('');
+  const [questionText, setQuestionText] = useState<string>(question);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const response = await fetch(`${API_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "question" : { "question": questionText} 
+      }),
+    });
+    const data: Response = await response.json();
+    setAnswer(data.answer);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuestionText(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="question">Question:</label>
+        <input 
+          onChange={handleInputChange} 
+          type="text" 
+          value={questionText} 
+          />
+        <button type="submit">Submit</button>
+      </form>
+
+      {answer && (
+        <div>
+          <h2>Answer:</h2>
+          <p>{answer}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default QuestionAnswer;
